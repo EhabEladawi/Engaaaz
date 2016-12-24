285cb89f27ca4c169e85ef639f9feddc85101bbb
Fernando Romany <FernandoRTF3@Gmail.com>
2016-12-24 22:24:54 +0200

    Done Ya Basha 10:24 - 24/12/2016

--- DentalClinic/adminprocess/historyadmin.Designer.cs
+++ DentalClinic/adminprocess/historyadmin.Designer.cs
@@ -36,6 +36,8 @@
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.radCheckedListBox1)).BeginInit();
             this.SuspendLayout();
@@ -43,7 +45,7 @@
             // pictureBox2
             // 
             this.pictureBox2.Image = global::DentalClinic.Properties.Resources._1;
-            this.pictureBox2.Location = new System.Drawing.Point(3, 63);
+            this.pictureBox2.Location = new System.Drawing.Point(-2, 53);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(332, 67);
             this.pictureBox2.TabIndex = 28;
@@ -51,10 +53,10 @@
             // 
             // radCheckedListBox1
             // 
-            this.radCheckedListBox1.Location = new System.Drawing.Point(273, 164);
+            this.radCheckedListBox1.Location = new System.Drawing.Point(329, 197);
             this.radCheckedListBox1.Name = "radCheckedListBox1";
-            this.radCheckedListBox1.Size = new System.Drawing.Size(217, 245);
-            this.radCheckedListBox1.TabIndex = 29;
+            this.radCheckedListBox1.Size = new System.Drawing.Size(324, 216);
+            this.radCheckedListBox1.TabIndex = 5;
             this.radCheckedListBox1.Text = "radCheckedListBox1";
             this.radCheckedListBox1.SelectedItemsChanged += new System.EventHandler(this.radCheckedListBox1_SelectedItemsChanged);
             this.radCheckedListBox1.SelectedItemChanged += new System.EventHandler(this.radCheckedListBox1_SelectedItemChanged);
@@ -63,52 +65,80 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(84, 164);
+            this.textBox1.Location = new System.Drawing.Point(111, 162);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(183, 20);
-            this.textBox1.TabIndex = 30;
+            this.textBox1.TabIndex = 1;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(14, 170);
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F);
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.label1.Location = new System.Drawing.Point(23, 164);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.Size = new System.Drawing.Size(54, 18);
             this.label1.TabIndex = 31;
             this.label1.Text = "Name";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(23, 386);
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.button1.Image = global::DentalClinic.Properties.Resources._7_button;
+            this.button1.Location = new System.Drawing.Point(26, 383);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
+            this.button1.Size = new System.Drawing.Size(95, 30);
+            this.button1.TabIndex = 2;
             this.button1.Text = "insert";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(111, 386);
+            this.button2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.button2.Image = global::DentalClinic.Properties.Resources._7_button;
+            this.button2.Location = new System.Drawing.Point(127, 383);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 32;
+            this.button2.Size = new System.Drawing.Size(95, 30);
+            this.button2.TabIndex = 6;
             this.button2.Text = "Update";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(192, 388);
+            this.button3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.button3.Image = global::DentalClinic.Properties.Resources._7_button;
+            this.button3.Location = new System.Drawing.Point(228, 383);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 33;
+            this.button3.Size = new System.Drawing.Size(95, 30);
+            this.button3.TabIndex = 7;
             this.button3.Text = "Delete";
             this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(44)))), ((int)(((byte)(62)))), ((int)(((byte)(80)))));
+            this.button4.Image = global::DentalClinic.Properties.Resources._7_button;
+            this.button4.Location = new System.Drawing.Point(558, 156);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(95, 30);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Search";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(329, 162);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(183, 20);
+            this.textBox2.TabIndex = 3;
             // 
             // historyadmin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(526, 434);
+            this.ClientSize = new System.Drawing.Size(670, 435);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -139,5 +169,7 @@
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
